462f4dcd13be97cce9e61131583e666261636a2f
Vusal27 <nuruva02@gmail.com>
2019-04-05 09:45:14 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { YMaps, Map } from "react-yandex-maps";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const mapState = {
+  center: [55.76, 37.64],
+  zoom: 10,
+  controls: []
+};
+
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.addressInput = React.createRef();
+	}
+
+	onYmapsLoad = ymaps => {
+		this.ymaps = ymaps;
+		var input = this.addressInput.current;
+		var MySuggestLayout = ymaps.templateLayoutFactory.createClass([
+			'{% if state.open %}',
+			  '{% include "islands#suggestView" %}',
+			'{% endif %}'    
+		  ].join(''))
+		input.addEventListener('keyup', () => {
+			
+			var suggestView = new this.ymaps.SuggestView(this.addressInput.current, {
+				layout: MySuggestLayout,
+				results: 6,
+				offset: [-2, 3],
+			});
+			suggestView.state.set('open', false);
+			if (input.value.length > 2) {
+				suggestView.state.set({
+					open: true,
+					panelClosed: false
+				});
+			} else if (input.value.length < 3) {
+				suggestView.state.set({
+					open: false,
+					panelClosed: true,
+					items: []
+				});
+			}
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<input id="suggest" ref={this.addressInput} placeholder="адрес..." />
+				<YMaps query={{ load: "package.full" }}>
+					<Map
+						state={mapState}
+						onLoad={this.onYmapsLoad}
+					/>
+				</YMaps>
+			</div>
+		);
+	}
 }
 
 export default App;
